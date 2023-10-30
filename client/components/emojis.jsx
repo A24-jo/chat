02d77a4c317bf8f1be5f39efb8c88d136ad662a5
A24-jo <bravo24ramosjose@gmail.com>
2023-10-29 23:31:57 -0500

    segundo

--- client/components/emojis.jsx
+++ client/components/emojis.jsx
@@ -1,31 +1,18 @@
 "use client"
 import EmojiPicker, {
     EmojiStyle,
-    Emoji,
 } from "emoji-picker-react";
-import { useState } from "react";
 
-export default function Emojis() {
-    const [selectedEmoji, setSelectedEmoji] = useState("1f60a");
-    const [inputValue, setInputValue] = useState("");
+export default function Emojis({setInput}) {
 
     function onClick(emojiData) {
-        setInputValue((inputValue) =>
-            inputValue + (emojiData.isCustom ? emojiData.unified : emojiData.emoji)
-        );
-        setSelectedEmoji(emojiData.unified);
+          setInput((prevInputValue) => ({
+            ...prevInputValue,
+            text: prevInputValue.text + (emojiData.isCustom ? emojiData.unified : emojiData.emoji)
+          }));
     }
-
     return (
         <div >
-            <div>
-                <input
-                    type="text"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                    placeholder="Select Emojis..."
-                />
-            </div>
             <EmojiPicker
                 onEmojiClick={onClick}
                 autoFocusSearch={false}
